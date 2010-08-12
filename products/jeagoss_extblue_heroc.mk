451dc1339ed64a83b4e85dec68df7f3d97bc7fc3
Jon Block <wjblock@gmail.com>
2010-08-12 17:22:40 -0400

    Add product files and vendorsetup for hero, heroc.

---
+++ products/jeagoss_extblue_heroc.mk
@@ -0,0 +1,12 @@
+# Inherit cyanogenmod configurations
+$(call inherit-product, vendor/cyanogen/products/cyanogen_heroc.mk)
+
+# Extremely Blue build time theming vendor setup
+# Add Extremely Blue Overrides
+PRODUCT_NAME := jeagoss_extblue_heroc
+PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay/common
+
+PRODUCT_COPY_FILES +=  \
+    vendor/jeagoss_extblue/proprietary/ThemeDefaults.apk:system/app/ThemeDefaults.apk
+PRODUCT_COPY_FILES +=  \
+    vendor/jeagoss_extblue/prebuilt/common/bin/setdefcols.sh:system/bin/setdefcols.sh
