9e4a6812b649dd9742190b3578da837e135ec4d0
Jef Oliver <jgoliver@jeago.com>
2010-08-14 02:54:34 -0600

    makefile fixes

--- products/jeagoss_extblue_heroc.mk
+++ products/jeagoss_extblue_heroc.mk
@@ -5,6 +5,7 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_heroc.mk)
 # Add Extremely Blue Overrides
 PRODUCT_NAME := jeagoss_extblue_heroc
 PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/dream_sapphire
 
 PRODUCT_COPY_FILES +=  \
     vendor/jeagoss_extblue/proprietary/ThemeDefaults.apk:system/app/ThemeDefaults.apk
